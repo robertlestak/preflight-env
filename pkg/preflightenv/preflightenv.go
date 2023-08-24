d3e21d2a5d9c95e149fc7566fd072a5b6ae3d565
robertlestak <robert.lestak@umusic.com>
2023-08-23 20:36:37 -0700

    print equivalent command

--- pkg/preflightenv/preflightenv.go
+++ pkg/preflightenv/preflightenv.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
@@ -48,11 +49,28 @@ func LoadConfig(filepath string) (*PreflightEnv, error) {
 	return pf, err
 }
 
+func (pf *PreflightEnv) Equivalent() {
+	l := Logger
+	l.Debug("printing equivalent command")
+	var cmd string
+	for k, v := range pf.EnvVars {
+		if v == "" {
+			cmd += `if [ -z "$` + k + `" ]; then echo "expecting ` + k + ` to exist" && exit 1; fi; `
+		} else {
+			cmd += `if [ "$` + k + `" != "` + v + `" ]; then echo "failed - expected: ` + v + `, got: $` + k + `" && exit 1; fi; `
+		}
+	}
+	cmd = strings.TrimSpace(cmd)
+	cmd = fmt.Sprintf("sh -c '%s'", cmd)
+	l.Infof("equivalent command: %s", cmd)
+}
+
 func (pf *PreflightEnv) Run() error {
 	l := Logger.WithFields(log.Fields{
 		"preflight": "env",
 	})
 	l.Debug("starting preflight-env")
+	pf.Equivalent()
 	for k, v := range pf.EnvVars {
 		if v == "" {
 			// checking if env var exists
